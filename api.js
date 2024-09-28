df30e18f0ac39d6e49c738ed1c34641cd1fcda2d
Nelly <n.babillon@ldlc.com>
2024-09-28 11:34:01 +0200

    ssss

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 console.log('👽')
 
 let api = fetch('https://pantagruweb.club/tentacules/webhook-test/b469b78f-40ba-437a-937d-48ba00985774')
-    .then(response => response.json())
+     .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
 
 console.log(api)
